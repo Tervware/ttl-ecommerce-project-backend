55a5c4523a6a8b212865dee73789399997b60aa4
terver <timothy.chahur@scelloo.com>
2019-12-08 18:54:04 +0100

    adds specs and category

--- models/Product.js
+++ models/Product.js
@@ -4,10 +4,17 @@ const Schema = mongoose.Schema;
 
 
 const ProductSchema = new Schema({
-    name:String,
-    image:String,
-    price:Number,
-    description:String,
+    name:{type:String},
+    image:{type:String},
+    price:{type:Number},
+    description:{type:String},
+    specification:{
+        type:Schema.Types.Mixed,
+       },
+    category:{
+        type:Schema.Types.ObjectId,
+        ref:'Category'
+    }
 });
 
 
