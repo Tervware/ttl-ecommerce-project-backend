cccd7141b524a82a63aa47442332665aebc8c50b
terver <timothy.chahur@scelloo.com>
2019-12-08 18:54:25 +0100

    Adds category routes

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,23 @@
 const express = require('express');
 
-const ProductsController = require('../controllers/ProductsController')
-
+const ProductsController = require('../controllers/ProductsController');
+const CategoriesController = require('../controllers/CategoriesController');
 const router = express.Router();
 
 router.get('/', function(req,res){
     res.send("Home");
 });
 
+// Products routes
 router.get('/products', ProductsController.getProducts);
 router.get('/products/:id', ProductsController.getProductById);
 router.post('/products', ProductsController.createProduct);
 router.patch('/products', ProductsController.updateProduct);
 router.delete('/products/:id', ProductsController.deleteProduct);
+
+//Categories routes
+router.post('/categories', CategoriesController.createCategory);
+router.get('/categories', CategoriesController.getCategories);
 
 
 module.exports = router;
